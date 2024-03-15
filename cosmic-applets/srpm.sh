92b34d41925d0e2ed41f5dff49a97dd462e2d2b9
Ryan Brue <ryanbrue@hotmail.com>
2024-03-14 23:44:44 -0500

    Cosmic applets

--- cosmic-applets/srpm.sh
+++ cosmic-applets/srpm.sh
@@ -5,11 +5,55 @@
 name='cosmic-applets'
 version='0.1.0'
 
-repo='https://pagure.io/fedora-cosmic/cosmic-packaging'
-path_to_spec='cosmic-applets/cosmic-applets.spec'
+path_to_spec='cosmic-applets/*'
+pop_repo='cosmic-applets'
+
+# Commit to target. Use "latest" if you want master
+commit="latest"
+
+# Don't edit anything past this line 
+# ===================================================== #
+
+LATEST="latest"
 
 cd ../..
 # We should be in the base directory now
+
+git clone --recurse-submodules https://github.com/pop-os/$pop_repo
+
+cd $pop_repo
+
+if [[ "$commit" == "$LATEST" ]]
+then
+    commit=$(git rev-parse HEAD)
+fi
+
+echo $commit
+short_commit=${commit:0:6}
+
+git reset --hard $commit
+
+mkdir .vendor
+
+cargo vendor > .vendor/config.toml
+
+tar -pcf vendor.tar vendor && mv vendor.tar ../vendor.tar
+
+rm -rf vendor && cd ..
+
+# Back into cosmic-packaging
+
+ls
+
+if [ "$pop_repo" != "$name" ]; then
+    mv $pop_repo $name
+else
+    echo "names are equal. continuing..."
+fi
+
+tar -czf $name.tar.gz $name
+
+rm -rf $name
 
 cp cosmic-packaging/$path_to_spec .
 
@@ -17,4 +61,4 @@ rm -rf cosmic-packaging
 
 current_date=$(date +'%Y%m%d.%H')
 
-sed -i "/^Version:    / s/.*/Version: $version~$current_date/" $name.spec
+sed -i "/^Version:    / s/.*/Version: $version~$current_date.$short_commit/" $name.spec

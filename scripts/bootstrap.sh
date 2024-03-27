10c70dd0a1d7e598283d111ded7ed4f4de1a65b3
Ryan Brue <ryanbrue@hotmail.com>
2024-03-26 20:39:48 -0500

    stuff

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+PACKAGE=
+
+REPO=https://github.com/pop-os/$PACKAGE
+
+git clone --recurse-submodules https://pagure.io/forks/ryanabx/fedora-cosmic/cosmic-packaging.git
+cp cosmic-packaging/rpms/$PACKAGE/* .
+cp cosmic-packaging/scripts/vendor-srpm.sh .
+. vendor-srpm.sh $PACKAGE 0.1.0 latest $PACKAGE.spec
---
+++ scripts/vendor-srpm.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -x
+
+# name of the crate/package
+name=$1
+# version of the crate/package
+version=$2
+# commit to target (latest == master)
+commit=$3
+# path to the spec file on the pc
+path_to_spec=$4
+# repo link
+repo=$5
+
+LATEST="latest"
+
+# Clone repo and cd into it
+mkdir $name && cd $name && git clone --recurse-submodules $repo .
+
+# Get latest commit hash if commit is set to latest
+if [[ "$commit" == "$LATEST" ]]
+then
+    commit=$(git rev-parse HEAD)
+fi
+
+# Short commit, used for versioning
+short_commit=${commit:0:6}
+
+# Reset to specified commit
+git reset --hard $commit
+
+# Vendor dependencies and zip vendor
+mkdir .vendor
+cargo vendor > .vendor/config.toml
+tar -pcJf $name-vendor-$version.tar.xz vendor && mv $name-vendor-$version.tar.xz ../$name-vendor-$version.tar.xz
+# Back into parent directory
+rm -rf vendor && cd ..
+
+# Zip source
+tar -czJf $name.tar.xz $name
+rm -rf $name
+
+# Get specfile
+cp $path_to_spec $name.spec 2>/dev/null || :
+
+# Make replacements to specfile
+sed -i "/^%global ver / s/.*/%global ver $version/" $name.spec
+sed -i "/^%global commit / s/.*/%global commit $commit/" $name.spec
+current_date=$(date +'%Y%m%d.%H')
+sed -i "/^%global date / s/.*/%global commit $current_date/" $name.spec

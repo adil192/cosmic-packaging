7bf96745c73a5001213ee5c8a2bf23e2ff95302c
Ryan Brue <ryanbrue@hotmail.com>
2024-03-27 15:49:38 -0500

    modify bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -x
 
 PACKAGE=
+VERSION=0.1.0
+COMMIT=latest
 REPO=https://github.com/pop-os/$PACKAGE
 
 git clone --recurse-submodules https://pagure.io/forks/ryanabx/fedora-cosmic/cosmic-packaging.git
 cp cosmic-packaging/rpms/$PACKAGE/* .
 cp cosmic-packaging/scripts/vendor-srpm.sh .
-. vendor-srpm.sh $PACKAGE 0.1.0 latest $PACKAGE.spec $REPO
+. vendor-srpm.sh $PACKAGE $VERSION $COMMIT $PACKAGE.spec $REPO

48d8e1c9dbd030a5a7bee3a004256b5e6f62c37c
Ryan Brue <ryanbrue@hotmail.com>
2024-05-27 22:00:15 -0500

    Update srpm.sh

--- scripts/srpm.sh
+++ scripts/srpm.sh
@@ -2,7 +2,7 @@
 
 # NAME of the crate/package
 NAME=$1
-SOURCE-NAME=$2
+SOURCE_NAME=$2
 # VERSION of the crate/package
 VERSION=$3
 # COMMIT to target (latest == master)
@@ -15,13 +15,13 @@ VENDOR=$6
 LATEST="LATEST"
 
 # Clone REPO and cd into it
-mkdir $NAME-$COMMIT && cd $NAME-$COMMIT && git clone --recurse-submodules $REPO .
+mkdir $SOURCE_NAME-$COMMIT && cd $SOURCE_NAME-$COMMIT && git clone --recurse-submodules $REPO .
 
 # Get latest COMMIT hash if COMMIT is set to latest
 if [[ "$COMMIT" == "$LATEST" ]]
 then
     COMMIT=$(git rev-parse HEAD)
-    cd .. && mv $NAME-latest $NAME-$COMMIT && cd $NAME-$COMMIT
+    cd .. && mv $SOURCE_NAME-latest $SOURCE_NAME-$COMMIT && cd $SOURCE_NAME-$COMMIT
 fi
 
 # Reset to specified COMMIT
@@ -31,7 +31,7 @@ if [ "$VENDOR" -eq 1 ]; then
     echo "VENDOR=1"
     # Vendor dependencies and zip vendor
     cargo vendor > ../vendor-config.toml
-    tar -pczf $NAME-$COMMIT-vendor.tar.gz vendor && mv $NAME-$COMMIT-vendor.tar.gz ../$NAME-$COMMIT-vendor.tar.gz
+    tar -pczf vendor.tar.gz vendor && mv vendor.tar.gz ../vendor.tar.gz
     # Back into parent directory
     rm -rf vendor
     cd ..
@@ -40,8 +40,8 @@ else
 fi
 
 # Zip SOURCE
-tar -pczf $NAME-$COMMIT.tar.xz $NAME-$COMMIT
-rm -rf $NAME-$COMMIT
+tar -pczf $SOURCE_NAME-$COMMIT.tar.xz $SOURCE_NAME-$COMMIT
+rm -rf $SOURCE_NAME-$COMMIT
 
 # Make replacements to specfile
 sed -i "/^%global ver / s/.*/%global ver $VERSION/" $NAME.spec
@@ -50,6 +50,6 @@ current_date=$(date +'%Y%m%d.%H')
 sed -i "/^%global date / s/.*/%global date $current_date/" $NAME.spec
 
 # Should have these sources
-# NAME-COMMIT.tar.gz
+# SOURCE_NAME-COMMIT.tar.gz
 # vendor.tar.gz
 # vendor-config.toml

3ed97841ca98cfe4c426cc9e5d968548bd8c2b1e
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-10-01 23:32:02 +0200

    feat: use the same version for nightly AND tagged in no-srpm

--- scripts/no-source-srpm.sh
+++ scripts/no-source-srpm.sh
@@ -5,6 +5,7 @@
 #
 # NAME: package name
 # VERSION: tag, semver
+# NIGHTLY: 0 or 1
 
 check_variable() {
     local var_name=$1
@@ -21,11 +22,5 @@ NIGHTLY=${NIGHTLY:-1}
 CURRENT_DATE=$(date +'%Y%m%d')
 
 # Make replacements to specfile
-if [ "$NIGHTLY" -eq 1 ]; then
-    echo "NIGHTLY=1"
-    sed -i "/^Version: / s/.*/Version:        ${VERSION}/" $NAME.spec
-else  
-    sed -i "/^Version: / s/.*/Version:        ${VERSION}^${CURRENT_DATE}/" $NAME.spec
-fi
-
+sed -i "/^Version: / s/.*/Version:        ${VERSION}^${CURRENT_DATE}/" $NAME.spec
 sed -i "/^%global cosmic_minver / s/.*/%global cosmic_minver $VERSION/" $NAME.spec

5f52748e411bc13d8b752d35c77d1b75976b7fda
Ryan Brue <ryanbrue.dev@gmail.com>
2025-01-14 16:39:04 -0600

    Bump tagged version to alpha.5.1
    Signed-off-by: Ryan Brue <ryanbrue.dev@gmail.com>

--- cosmic-packaging-tool/extra_scripts/build.sh
+++ cosmic-packaging-tool/extra_scripts/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Define a list of strings
+# list=("cosmic-app-library" "cosmic-applets" "cosmic-bg" "cosmic-comp" "cosmic-edit" "cosmic-files" "cosmic-greeter" "cosmic-icon-theme" "cosmic-idle" "cosmic-launcher" "cosmic-notifications" "cosmic-osd" "cosmic-panel" "cosmic-player" "cosmic-randr" "cosmic-screenshot" "cosmic-session" "cosmic-settings" "cosmic-settings-daemon" "cosmic-store" "cosmic-term" "cosmic-wallpapers" "cosmic-workspaces" "xdg-desktop-portal-cosmic")
 # list=("cosmic-app-library" "cosmic-applets" "cosmic-bg" "cosmic-comp" "cosmic-edit" "cosmic-files" "cosmic-greeter" "cosmic-icon-theme" "cosmic-idle" "cosmic-launcher" "cosmic-notifications" "cosmic-osd" "cosmic-panel" "cosmic-randr" "cosmic-screenshot" "cosmic-session" "cosmic-settings" "cosmic-settings-daemon" "cosmic-store" "cosmic-term" "cosmic-wallpapers" "cosmic-workspaces" "xdg-desktop-portal-cosmic")
+list=("cosmic-player")
+# NOTE: Rebuild pop-launcher as well!
 # list=("cosmic-edit" "cosmic-files" "cosmic-greeter" "cosmic-icon-theme" "cosmic-idle" "cosmic-launcher" "cosmic-notifications" "cosmic-osd" "cosmic-panel" "cosmic-randr" "cosmic-screenshot" "cosmic-session" "cosmic-settings" "cosmic-settings-daemon" "cosmic-store" "cosmic-term" "cosmic-wallpapers" "cosmic-workspaces" "xdg-desktop-portal-cosmic")
 
-list=("cosmic-settings" "cosmic-settings-daemon")
+# list=("cosmic-settings" "cosmic-settings-daemon")
 
 # Loop through the list
 for item in "${list[@]}"
@@ -12,7 +15,7 @@ do
     # Run a command for each string
     echo "Processing: $item"
     rm -rf ~/workdir && mkdir -p ~/workdir
-    cargo run -- setup-build ~/workdir $item --auto-srpm --version 1.0.0~alpha.5
+    cargo run -- setup-build ~/workdir $item --auto-srpm --version 1.0.0~alpha.5.1
     rm -rf ~/workdir && mkdir -p ~/workdir
     cargo run -- setup-build ~/workdir $item --build-branch f41 --source-branch rawhide
 done
--- cosmic-packaging-tool/src/main.rs
+++ cosmic-packaging-tool/src/main.rs
@@ -66,7 +66,7 @@ enum Commands {
         /// Optional source branch to get content from
         #[arg(long)]
         source_branch: Option<String>,
-        /// Version (i.e. 1.0.0~alpha.5)
+        /// Version (i.e. 1.0.0~alpha.5.1)
         #[arg(long)]
         version: Option<String>,
     },

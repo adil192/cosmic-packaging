bb619851ce7d30d46073f53677ff8693a1f2c105
Ryan Brue <ryanbrue.dev@gmail.com>
2025-04-24 17:24:14 -0500

    s
    Signed-off-by: Ryan Brue <ryanbrue.dev@gmail.com>

--- cosmic-packaging-tool/extra_scripts/build.sh
+++ cosmic-packaging-tool/extra_scripts/build.sh
@@ -2,7 +2,8 @@
 
 list=("cosmic-app-library" "cosmic-applets" "cosmic-bg" "cosmic-comp" "cosmic-edit" "cosmic-files" "cosmic-greeter" "cosmic-icon-theme" "cosmic-idle" "cosmic-launcher" "cosmic-notifications" "cosmic-osd" "cosmic-panel" "cosmic-player" "cosmic-randr" "cosmic-screenshot" "cosmic-session" "cosmic-settings" "cosmic-settings-daemon" "cosmic-store" "cosmic-term" "cosmic-wallpapers" "cosmic-workspaces" "pop-launcher" "xdg-desktop-portal-cosmic")
 
-read "WARNING: This will erase everything in ~/workdir. Are you sure about this?"
+echo "WARNING: This will erase everything in ~/workdir. Are you sure about this?"
+read v
 
 for item in "${list[@]}"
 do

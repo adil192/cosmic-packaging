e9a6bf9f0d84a717deeb1309c689c48cbce49953
Ryan Brue <ryanbrue.dev@gmail.com>
2025-09-24 20:56:18 -0500

    Update script to be parallel
    Signed-off-by: Ryan Brue <ryanbrue.dev@gmail.com>

--- scripts/build-new-packages-for-release.sh
+++ scripts/build-new-packages-for-release.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
-WORKDIR=~/workdir
-TAG=1.0.0~beta.1
-
 # list=("cosmic-app-library" "cosmic-applets" "cosmic-bg" "cosmic-comp" "cosmic-edit" "cosmic-files" "cosmic-greeter" "cosmic-icon-theme" "cosmic-idle" "cosmic-launcher" "cosmic-notifications" "cosmic-osd" "cosmic-panel" "cosmic-player" "cosmic-randr" "cosmic-screenshot" "cosmic-session" "cosmic-settings" "cosmic-settings-daemon" "cosmic-store" "cosmic-term" "cosmic-wallpapers" "cosmic-workspaces" "pop-launcher" "xdg-desktop-portal-cosmic")
 list=("cosmic-applets" "cosmic-bg" "cosmic-comp" "cosmic-edit" "cosmic-files" "cosmic-greeter" "cosmic-icon-theme" "cosmic-idle" "cosmic-launcher" "cosmic-notifications" "cosmic-osd" "cosmic-panel" "cosmic-player" "cosmic-randr" "cosmic-screenshot" "cosmic-session" "cosmic-settings" "cosmic-settings-daemon" "cosmic-store" "cosmic-term" "cosmic-wallpapers" "cosmic-workspaces" "pop-launcher" "xdg-desktop-portal-cosmic")
 
+MAX_JOBS=5
+
+function build_package() {
+    pkg=$1
+    echo "======================================"
+    echo "Processing: $pkg"
+    python3 ./scripts/cosmic-packaging-new-release.py $pkg all
+    rm -f ~/workdir/$pkg.src.rpm
+    rm -rf ~/workdir/$pkg/
+    echo "======================================"
+}
+
 for item in "${list[@]}"
 do
-    echo "Processing: $item"
-    python3 ./scripts/cosmic-packaging-new-release.py $item all
-    rm -f ~/workdir/$item.src.rpm
-    rm -rf ~/workdir/$item/
+    build_package $item &
+    ((count++))
+
+    if (( count % MAX_JOBS == 0 )); then
+        wait
+    fi
 done
+
+wait 

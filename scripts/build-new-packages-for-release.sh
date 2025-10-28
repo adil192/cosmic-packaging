1a441c1e76f6a14fa821c8643974d79a2585c56b
Ryan Brue <ryanbrue.dev@gmail.com>
2025-10-28 00:59:07 -0500

    Update build-new-packages-for-release.sh

--- scripts/build-new-packages-for-release.sh
+++ scripts/build-new-packages-for-release.sh
@@ -1,15 +1,41 @@
 #!/bin/sh
 
-# list=("cosmic-app-library" "cosmic-applets" "cosmic-bg" "cosmic-comp" "cosmic-edit" "cosmic-files" "cosmic-greeter" "cosmic-icon-theme" "cosmic-idle" "cosmic-launcher" "cosmic-notifications" "cosmic-osd" "cosmic-panel" "cosmic-player" "cosmic-randr" "cosmic-screenshot" "cosmic-session" "cosmic-settings" "cosmic-settings-daemon" "cosmic-store" "cosmic-term" "cosmic-wallpapers" "cosmic-workspaces" "pop-launcher" "xdg-desktop-portal-cosmic")
-list=("cosmic-applets" "cosmic-bg" "cosmic-comp" "cosmic-edit" "cosmic-files" "cosmic-greeter" "cosmic-icon-theme" "cosmic-idle" "cosmic-launcher" "cosmic-notifications" "cosmic-osd" "cosmic-panel" "cosmic-player" "cosmic-randr" "cosmic-screenshot" "cosmic-session" "cosmic-settings" "cosmic-settings-daemon" "cosmic-store" "cosmic-term" "cosmic-wallpapers" "cosmic-workspaces" "pop-launcher" "xdg-desktop-portal-cosmic")
+list=(
+    "cosmic-app-library"
+    "cosmic-applets"
+    "cosmic-bg"
+    "cosmic-comp"
+    "cosmic-edit"
+    "cosmic-files"
+    "cosmic-greeter"
+    "cosmic-icon-theme"
+    "cosmic-idle"
+    "cosmic-launcher"
+    "cosmic-notifications"
+    "cosmic-osd"
+    "cosmic-panel"
+    "cosmic-player"
+    "cosmic-randr"
+    "cosmic-screenshot"
+    "cosmic-session"
+    "cosmic-settings"
+    "cosmic-settings-daemon"
+    "cosmic-store"
+    "cosmic-term"
+    "cosmic-wallpapers"
+    "cosmic-workspaces"
+    "pop-launcher"
+    "xdg-desktop-portal-cosmic"
+    )
+# list=("cosmic-applets" "cosmic-bg" "cosmic-comp" "cosmic-edit" "cosmic-files" "cosmic-greeter" "cosmic-icon-theme" "cosmic-idle" "cosmic-launcher" "cosmic-notifications" "cosmic-osd" "cosmic-panel" "cosmic-player" "cosmic-randr" "cosmic-screenshot" "cosmic-session" "cosmic-settings" "cosmic-settings-daemon" "cosmic-store" "cosmic-term" "cosmic-wallpapers" "cosmic-workspaces" "pop-launcher" "xdg-desktop-portal-cosmic")
 
-MAX_JOBS=5
+MAX_JOBS=10
 
 function build_package() {
     pkg=$1
     echo "======================================"
     echo "Processing: $pkg"
-    python3 ./scripts/cosmic-packaging-new-release.py $pkg all
+    python3 ./scripts/cosmic-packaging-new-release.py $pkg all --side-tag f44-build-side-121606
     rm -f ~/workdir/$pkg.src.rpm
     rm -rf ~/workdir/$pkg/
     echo "======================================"

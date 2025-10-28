28b0da740e7f3a53f01a17f53e1fa4422c1fcb64
Ryan Brue <ryanbrue.dev@gmail.com>
2025-10-28 14:33:02 -0500

    Update script

--- scripts/build-new-packages-for-release.sh
+++ scripts/build-new-packages-for-release.sh
@@ -29,11 +29,16 @@ list=(
     )
 # list=("cosmic-applets" "cosmic-bg" "cosmic-comp" "cosmic-edit" "cosmic-files" "cosmic-greeter" "cosmic-icon-theme" "cosmic-idle" "cosmic-launcher" "cosmic-notifications" "cosmic-osd" "cosmic-panel" "cosmic-player" "cosmic-randr" "cosmic-screenshot" "cosmic-session" "cosmic-settings" "cosmic-settings-daemon" "cosmic-store" "cosmic-term" "cosmic-wallpapers" "cosmic-workspaces" "pop-launcher" "xdg-desktop-portal-cosmic")
 
+rm -rf ./.out
+mkdir -p ./.out
+
 function build_package() {
     pkg=$1
     echo "======================================"
     echo "Processing: $pkg"
-    python3 ./scripts/cosmic-packaging-new-release.py $pkg all --side-tag f44-build-side-121606
+    rm -f ~/workdir/$pkg.src.rpm
+    rm -rf ~/workdir/$pkg/
+    python3 ./scripts/cosmic-packaging-new-release.py $pkg > ./.out/log-$item.txt
     rm -f ~/workdir/$pkg.src.rpm
     rm -rf ~/workdir/$pkg/
     echo "======================================"
@@ -41,5 +46,5 @@ function build_package() {
 
 for item in "${list[@]}"
 do
-    build_package $item > ./.out/log-$item.txt
+    build_package $item
 done

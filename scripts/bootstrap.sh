9ee2caf4138255a5d890df05fd3727a2a41f0031
Ryan Brue <ryanbrue.dev@gmail.com>
2026-02-24 21:30:22 +0000

    chore: don't copy script before executing it
    Copied from upstream's copr

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,11 +7,9 @@ RPM_REPO_NAME=cosmic-packaging
 RPM_REPO=https://forge.fedoraproject.org/cosmic/$RPM_REPO_NAME.git
 
 git clone $RPM_REPO
-cp $RPM_REPO_NAME/scripts/$SCRIPT .
 
-python3 $SCRIPT $NAME --input $(pwd)/cosmic-packaging --output $(pwd)
+python3 $RPM_REPO_NAME/scripts/$SCRIPT $NAME --input $(pwd)/cosmic-packaging --output $(pwd)
 
-rm $SCRIPT
 rm -rf $RPM_REPO_NAME
 
 ls -a
